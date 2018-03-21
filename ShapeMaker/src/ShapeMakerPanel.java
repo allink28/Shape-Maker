f5739944fdff1cace16ae46ae887a890b3b90c35
Allen <allink28@gmail.com>
2018-03-20 21:31:15 -0400

    Update code style and introduce String switch statement

--- ShapeMaker/src/ShapeMakerPanel.java
+++ ShapeMaker/src/ShapeMakerPanel.java
@@ -16,16 +16,16 @@ import javax.swing.JPanel;
 
 
 // -------------------------------------------------------------------------
+
 /**
- *  This class holds the primary panel which contains the controls panel
- *  and drawing area.
+ * This class holds the primary panel which contains the controls panel
+ * and drawing area.
  *
- *  @author Allen Preville (a892186)
- *  @version Feb 24, 2010
+ * @author Allen Preville (a892186)
+ * @version Feb 24, 2010
  */
 
-public class ShapeMakerPanel extends JPanel
-{
+public class ShapeMakerPanel extends JPanel {
     private JComboBox shapeChoice;
     private JPanel currentColor;
     private JCheckBox filled;
@@ -45,10 +45,9 @@ public class ShapeMakerPanel extends JPanel
     /**
      * Constructor for ShapeMakerPanel
      */
-    public ShapeMakerPanel()
-    {
+    public ShapeMakerPanel() {
         shapeChoice = new JComboBox();
-        shapeChoice.setName( "shapeChoice" );
+        shapeChoice.setName("shapeChoice");
         shapeChoice.insertItemAt("Line", 0);
         shapeChoice.insertItemAt("Rectangle", 1);
         shapeChoice.insertItemAt("Square", 2);
@@ -59,177 +58,172 @@ public class ShapeMakerPanel extends JPanel
         ControlListener listener2 = new ControlListener();
 
         currentColor = new JPanel();
-        currentColor.setName( "currentColor" );
+        currentColor.setName("currentColor");
         currentColor.setPreferredSize(new Dimension(35, 25));
         currentColor.setBackground(Color.black);
         filled = new JCheckBox("Filled");
-        filled.setName( "filled" );
-        undo = new JButton( "Undo" );
-        undo.setName( "undo" );
-        clear = new JButton( "Clear" );
-        clear.setName( "clear" );
+        filled.setName("filled");
+        undo = new JButton("Undo");
+        undo.setName("undo");
+        clear = new JButton("Clear");
+        clear.setName("clear");
 
         setPreferredSize(new Dimension(400, 450));
 
         controls = new JPanel();
-        controls.setName( "controls" );
-        controls.add( shapeChoice );
-        controls.add( currentColor );
-        controls.add( filled );
-        controls.add( undo );
-        controls.add( clear );
+        controls.setName("controls");
+        controls.add(shapeChoice);
+        controls.add(currentColor);
+        controls.add(filled);
+        controls.add(undo);
+        controls.add(clear);
         controls.setPreferredSize(new Dimension(400, 50));
 
         drawingArea = new JPanel();
-        drawingArea.setName( "drawingArea" );
+        drawingArea.setName("drawingArea");
         drawingArea.setBackground(Color.white);
         drawingArea.setPreferredSize(new Dimension(400, 400));
-        shape = new Line( null, null, color); 
+        shape = new Line(null, null, color);
 
-        add( controls );
+        add(controls);
         addMouseListener(listener1);
         addMouseMotionListener(listener1);
         currentColor.addMouseListener(listener1);
-        undo.addMouseListener( listener1 );
-        clear.addMouseListener( listener1 );
+        undo.addMouseListener(listener1);
+        clear.addMouseListener(listener1);
         filled.addItemListener(listener2);
-        shapeChoice.addItemListener( listener2 );
+        shapeChoice.addItemListener(listener2);
     }
 
     /**
      * Draws all the shapes displayed.
+     *
      * @param g The graphic context.
      */
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (!list.isEmpty())
-        {
-            for ( Shape s : list )
-            {
+        if (!list.isEmpty()) {
+            for (Shape s : list) {
                 s.draw(g);
             }
         }
-        if (point1 != null && obj != null)
-        {
+        if (point1 != null && obj != null) {
             shape.draw(g);
         }
     }
 
     /**
      * Returns the list of shapes to be drawn and maintained on the panel.
+     *
      * @return list The list of shapes.
      */
-    public List<Shape> getShapes()
-    {
+    public List<Shape> getShapes() {
         return list;
     }
+
     /**
      * This method returns the starting point of the current shape.
+     *
      * @return point1 The starting point of the shape.
      */
-    public Point getStartPoint()
-    {
+    public Point getStartPoint() {
         return shape.getPoint1();
     }
+
     /**
      * This method returns the ending point of the current shape.
+     *
      * @return point2 The ending point of the shape.
      */
-    public Point getEndPoint()
-    {
+    public Point getEndPoint() {
         return shape.getPoint2();
     }
+
     /**
      * This method gets the status of the checkbox.
+     *
      * @return checkbox The status of the checkbox. True if checked.
      */
-    public boolean getFilledStatus()
-    {
+    public boolean getFilledStatus() {
         return checkbox;
     }
     // ----------------------------------------------------------
+
     /**
      * Returns color selected.
+     *
      * @return Returns the color of the shapes.
      */
-    public Color getColor()
-    {
+    public Color getColor() {
         return shape.getColor();
     }
 
     /**
      * Represents the listener for Mouse events.
      */
-    private class ShapeListener extends MouseAdapter
-    {
+    private class ShapeListener extends MouseAdapter {
         // ----------------------------------------------------------
+
         /**
          * Captures the initial position at which the mouse button is
          * pressed.
+         *
          * @param event The event to be handled
          */
-        public void mousePressed(MouseEvent event)
-        {
-            if ( event.getSource() != controls && obj != null)
-            {
-                if ( obj.toString().equals( "Line" ))
-                {
-                    shape = new Line( point1, point2, color);
-                }
-                else if ( obj.toString().equals( "Rectangle" ))
-                {
-                    shape = new Rectangle( point1, point2, color);
-                }
-                else if ( obj.toString().equals( "Oval" ))
-                {
-                    shape = new Oval( point1, point2, color);
-                }
-                else if ( obj.toString().equals( "Square" ))
-                {
-                    shape = new Square( point1, point2, color);
-                }
-                else if ( obj.toString().equals( "Circle" ))
-                {
-                    shape = new Circle( point1, point2, color);
+        public void mousePressed(MouseEvent event) {
+            if (event.getSource() != controls && obj != null) {
+
+                switch (obj.toString()) {
+                    case "Line":
+                        shape = new Line(point1, point2, color);
+                        break;
+                    case "Rectangle":
+                        shape = new Rectangle(point1, point2, color);
+                        break;
+                    case "Oval":
+                        shape = new Oval(point1, point2, color);
+                        break;
+                    case "Square":
+                        shape = new Square(point1, point2, color);
+                        break;
+                    case "Circle":
+                        shape = new Circle(point1, point2, color);
                 }
 
-                if (event.getPoint().y > 25)
-                {
-                    shape.setFilled( checkbox );
+                if (event.getPoint().y > 25) {
+                    shape.setFilled(checkbox);
                     point1 = event.getPoint();
-                    shape.setPoint1( point1 );
+                    shape.setPoint1(point1);
                 }
             }
         }
 
-     // ----------------------------------------------------------
+        // ----------------------------------------------------------
+
         /**
          * Gets the current position of the mouse as it is dragged and
          * redraws the shape to create a rubberband effect.
+         *
          * @param event The event to be handled
          */
-        public void mouseDragged(MouseEvent event)
-        {
-            if ( event.getSource() != controls && obj != null
-                && point1 != null)
-            {
+        public void mouseDragged(MouseEvent event) {
+            if (event.getSource() != controls && obj != null
+                    && point1 != null) {
 
                 point2 = event.getPoint();
-                shape.setPoint2( point2 );
+                shape.setPoint2(point2);
                 repaint();
             }
         }
 
         /**
          * Adds current shape to list when the mouse is released.
+         *
          * @param event The event to be handled
          */
-        public void mouseReleased(MouseEvent event)
-        {
-            if (point1 != null)
-            {
-                list.add( shape );
+        public void mouseReleased(MouseEvent event) {
+            if (point1 != null) {
+                list.add(shape);
                 point1 = null;
                 point2 = null;
                 repaint();
@@ -238,24 +232,19 @@ public class ShapeMakerPanel extends JPanel
 
         /**
          * Activates the component clicked.
+         *
          * @param event The mouse click event to be handled.
          */
-        public void mouseClicked(MouseEvent event)
-        {
-            if ( event.getSource() == currentColor)
-            {
-                color = JColorChooser.showDialog( shapeChoice,
-                    "Color Chooser", Color.black );
-                currentColor.setBackground( color );
-            }
-            else if ( event.getSource() == clear )
-            {
+        public void mouseClicked(MouseEvent event) {
+            if (event.getSource() == currentColor) {
+                color = JColorChooser.showDialog(shapeChoice,
+                        "Color Chooser", Color.black);
+                currentColor.setBackground(color);
+            } else if (event.getSource() == clear) {
                 list.clear();
                 repaint();
-            }
-            else if ( list.size() > 0)
-            {
-                list.remove( list.size() - 1 );
+            } else if (list.size() > 0) {
+                list.remove(list.size() - 1);
                 repaint();
             }
         }
@@ -264,20 +253,16 @@ public class ShapeMakerPanel extends JPanel
     /**
      * Represents the listener for all controls.
      */
-    private class ControlListener implements ItemListener
-    {
+    private class ControlListener implements ItemListener {
         /**
          * Activates when the combo box or check box state changes.
+         *
          * @param e The event to be handled.
          */
-        public void itemStateChanged( ItemEvent e )
-        {
-            if ( e.getSource() == shapeChoice)
-            {
+        public void itemStateChanged(ItemEvent e) {
+            if (e.getSource() == shapeChoice) {
                 obj = shapeChoice.getSelectedItem();
-            }
-            else
-            {
+            } else {
                 checkbox = !checkbox;
             }
         }
